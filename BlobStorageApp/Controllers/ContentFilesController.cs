d5c93f496585dc24c9ef21455c3bff6562495836
Andrew <andrewdfabrizio@gmail.com>
2021-03-04 00:26:31 -0500

    Added Post Method - Not Tested

---
+++ BlobStorageApp/Controllers/ContentFilesController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Storage.Blob;
+using BlobStorageApp.Repositories;
+using BlobStorageApp.DataTransferObjects;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlobStorageApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentFilesController : ControllerBase
+    {
+
+        private readonly IStorageRepository _storageRepository;
+
+        /// <summary>
+        /// Returns the list of files available for download
+        /// </summary>
+        /// <returns>The list of files available for download</returns>
+        public ContentFilesController(IStorageRepository storageRepository)
+        {
+            _storageRepository = storageRepository;
+        }
+
+        /// <summary>
+        /// Gets or sets the configuration.
+        /// </summary>
+        /// <value>
+        /// The configuration.
+        /// </value>
+        public IConfiguration Configuration { get; set; }
+
+        /// <summary>
+        /// Gets the storage connection string.
+        /// </summary>
+        /// <value>
+        /// The storage connection string.
+        /// </value>
+        public string StorageConnectionString
+        {
+            get
+            {
+                return Configuration.GetConnectionString("DefaultConnection");
+            }
+        }
+
+        // PUT api/<ContentFilesController>/5
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(List<ErrorResponse>), (int)HttpStatusCode.BadRequest)]
+        [Route("/api/v1/{containerName}/contentfiles/{fileName}")]
+        [HttpPut("{noteName}")]
+        public async Task<IActionResult> UploadFile([FromRoute]string containerName, [FromRoute] string fileName, [FromBody]IFormFile fileData)
+        {
+            // Get the Cloud Storage Account
+            CloudStorageAccount Account = CloudStorageAccount.Parse(StorageConnectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = Account.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container. 
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            // Create the container if it doesn't already exist.
+            await container.CreateIfNotExistsAsync();
+
+            if (containerName.ToLower().Contains("public")) {
+
+                // Set permissions on the blob container to ALLOW public access
+                await container.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            }
+
+            // IS BLOB STORAGE PRIVATE BY DEFAULT?
+
+            // Retrieve reference to a blob named the blob specified by the caller
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+
+            // Create or overwrite the blob with contents of the message provided
+            using Stream stream = fileData.OpenReadStream();
+            await _storageRepository.UploadFile(fileData.FileName, stream, fileData.ContentType);
+
+            return CreatedAtRoute("GetFileByIdRoute", null);
+        }
+
+        // GET: api/<ContentFilesController>
+        [Route("{id}", Name = "GetFileByIdRoute")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ContentFilesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ContentFilesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ContentFilesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ContentFilesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BlobStorageApp/DataTransferObjects/ErrorResponse.cs
@@ -0,0 +1,74 @@
+﻿namespace BlobStorageApp.DataTransferObjects
+{
+    /// <summary>
+    /// Defines the standard error format to be returned to the client.
+    /// </summary>
+    public class ErrorResponse
+    {
+        public int errorNumber { get; set; }
+        public string parameterName { get; set; }
+        public string parameterValue { get; set; }
+        public string errorDescription { get; set; }
+
+        /// <summary>
+        /// Converts an error number inside an encoded error description, to the standard error number
+        /// </summary>
+        /// <param name="encodedErrorDescription">The error description</param>
+        /// <returns>The decoded error number</returns>
+        public static int GetErrorNumberFromDescription(string encodedErrorDescription)
+        {
+            if (int.TryParse(encodedErrorDescription, out int errorNumber))
+            {
+                return errorNumber;
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Converts an error number inside an encoded error description, to the standard error response
+        /// </summary>
+        /// <param name="encodedErrorDescription">The error description</param>
+        /// <returns>The decoded error message and number</returns>
+        public static (string decodedErrorMessage, int decodedErrorNumber) GetErrorMessage(string encodedErrorDescription)
+        {
+
+            int errorNumber = GetErrorNumberFromDescription(encodedErrorDescription);
+
+            switch (errorNumber)
+            {
+                case 1:
+                    {
+                        return ("The entity already exists.", errorNumber);
+                    }
+                case 2:
+                    {
+                        return ("The parameter value is too large.", errorNumber);
+                    }
+                case 3:
+                    {
+                        return ("The parameter is required.", errorNumber);
+                    }
+                case 4:
+                    {
+                        return ("The maximum number of entities have been created. No further entities can be created at this time.", errorNumber);
+                    }
+                case 5:
+                    {
+                        return ("The entity could not be found.", errorNumber);
+                    }
+                case 6:
+                    {
+                        return ("The parameter value is too small.", errorNumber);
+                    }
+                case 7:
+                    {
+                        return ("The parameter value is not valid.", errorNumber);
+                    }
+                default:
+                    {
+                        return ($"Raw Error: {encodedErrorDescription}", errorNumber);
+                    }
+            }
+        }
+    }
+}
