e893f1d4e35316dad4331a0feb59d189c7b3160b
Andrew <andrewdfabrizio@gmail.com>
2021-03-03 23:34:44 -0500

    Added Exceptions, Repositories, and Settings Directories

---
+++ BlobStorageApp/Repositories/IStorageRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlobStorageApp.Repositories
+{
+    public interface IStorageRepository
+    {
+        Task DeleteFile(string fileName);
+        Task<(MemoryStream fileStream, string contentType)> GetFileAsync(string fileName);
+        Task<byte[]> GetFileInByteArrayAsync(string fileName);
+        Task<List<string>> GetListOfBlobs();
+        Task UploadFile(string fileName, Stream fileStream, string contentType);
+    }
+}
---
+++ BlobStorageApp/Settings/PictureSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace BlobStorageApp.Settings
+{
+    /// <summary>
+    /// Implements the picture settings 
+    /// </summary>
+    public class PictureSettings : IPictureSettings
+    {
+        /// <summary>
+        /// The picures storage container name
+        /// </summary>
+        public string PictureContainerName { get; set; }
+    }
+}
